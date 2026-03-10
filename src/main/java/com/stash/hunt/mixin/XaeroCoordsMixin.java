e55a79d96e08f721bc58bc30d2436f1dfea3b750
benjamin920101 <47590892+benjamin920101@users.noreply.github.com>
2026-03-10 14:55:58 +0800

    Update XaeroCoordsMixin.java

--- src/main/java/com/stash/hunt/mixin/XaeroCoordsMixin.java
+++ src/main/java/com/stash/hunt/mixin/XaeroCoordsMixin.java
@@ -1,7 +1,7 @@
 package com.stash.hunt.mixin;
 
 import com.stash.hunt.MagicMix;
-import net.minecraft.client.gui.FontRenderer;
+import net.minecraft.client.font.TextRenderer;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Redirect;
@@ -10,12 +10,12 @@ import xaeroplus.module.impl.Drawing;
 @Mixin(Drawing.class)
 public class XaeroCoordsMixin {
 
-    @Redirect(method = "render", at = @At(value = "INVOKE", target = "Lnet/minecraft/client/gui/FontRenderer;drawString(Ljava/lang/String;III)I"), remap = false)
-    private int replaceCoordString(FontRenderer fr, String text, int x, int y, int color) {
+    @Redirect(method = "render", at = @At(value = "INVOKE", target = "Lnet/minecraft/client/font/TextRenderer;draw(Ljava/lang/String;FFI)I"))
+    private int replaceCoordString(TextRenderer fr, String text, float x, float y, int color) {
         if (MagicMix.coordinatesIsActive()) {
             String fake = String.format("X: %d  Z: %d", (int) MagicMix.getX(), (int) MagicMix.getZ());
-            return fr.drawString(fake, x, y, color);
+            return fr.draw(fake, x, y, color);
         }
-        return fr.drawString(text, x, y, color);
+        return fr.draw(text, x, y, color);
     }
 }
