fed295ab51314b8827582808f13b4c13a3561bb4
benjamin920101 <47590892+benjamin920101@users.noreply.github.com>
2026-03-10 15:16:17 +0800

    Update XaeroCoordsMixin.java

--- src/main/java/com/stash/hunt/mixin/XaeroCoordsMixin.java
+++ src/main/java/com/stash/hunt/mixin/XaeroCoordsMixin.java
@@ -2,6 +2,7 @@ package com.stash.hunt.mixin;
 
 import com.stash.hunt.MagicMix;
 import net.minecraft.client.font.TextRenderer;
+import net.minecraft.text.OrderedText;
 import org.joml.Matrix4f;
 import net.minecraft.client.render.VertexConsumerProvider;
 import org.spongepowered.asm.mixin.Mixin;
@@ -14,6 +15,15 @@ public class XaeroCoordsMixin {
 
     @Redirect(method = "render", at = @At(value = "INVOKE", target = "Lnet/minecraft/client/font/TextRenderer;draw(Ljava/lang/String;FFIZLorg/joml/Matrix4f;Lnet/minecraft/client/render/VertexConsumerProvider;Lnet/minecraft/client/font/TextRenderer$TextLayerType;II)I"))
     private int replaceCoordString(TextRenderer fr, String text, float x, float y, int color, boolean shadow, Matrix4f matrix, VertexConsumerProvider provider, TextRenderer.TextLayerType type, int light, int overlay) {
+        if (MagicMix.coordinatesIsActive()) {
+            String fake = String.format("X: %d  Z: %d", (int) MagicMix.getX(), (int) MagicMix.getZ());
+            return fr.draw(fake, x, y, color, shadow, matrix, provider, type, light, overlay);
+        }
+        return fr.draw(text, x, y, color, shadow, matrix, provider, type, light, overlay);
+    }
+
+    @Redirect(method = "render", at = @At(value = "INVOKE", target = "Lnet/minecraft/client/font/TextRenderer;draw(Lnet/minecraft/text/OrderedText;FFIZLorg/joml/Matrix4f;Lnet/minecraft/client/render/VertexConsumerProvider;Lnet/minecraft/client/font/TextRenderer$TextLayerType;II)I"))
+    private int replaceCoordStringOrdered(TextRenderer fr, OrderedText text, float x, float y, int color, boolean shadow, Matrix4f matrix, VertexConsumerProvider provider, TextRenderer.TextLayerType type, int light, int overlay) {
         if (MagicMix.coordinatesIsActive()) {
             String fake = String.format("X: %d  Z: %d", (int) MagicMix.getX(), (int) MagicMix.getZ());
             return fr.draw(fake, x, y, color, shadow, matrix, provider, type, light, overlay);
