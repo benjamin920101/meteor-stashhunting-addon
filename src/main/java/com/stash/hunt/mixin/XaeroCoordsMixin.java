97db9105d8ff56b262677a3fee3510c2952e9e05
benjamin920101 <47590892+benjamin920101@users.noreply.github.com>
2026-03-10 15:44:08 +0800

    Update XaeroCoordsMixin.java

--- src/main/java/com/stash/hunt/mixin/XaeroCoordsMixin.java
+++ src/main/java/com/stash/hunt/mixin/XaeroCoordsMixin.java
@@ -3,7 +3,7 @@ package com.stash.hunt.mixin;
 import com.stash.hunt.MagicMix;
 import net.minecraft.client.font.TextRenderer;
 import net.minecraft.text.OrderedText;
-import org.joml.Matrix4f;
+import net.minecraft.client.util.math.Matrix4f;
 import net.minecraft.client.render.VertexConsumerProvider;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
@@ -13,7 +13,7 @@ import xaeroplus.module.impl.Drawing;
 @Mixin(Drawing.class)
 public class XaeroCoordsMixin {
 
-    @Redirect(method = "render", at = @At(value = "INVOKE", target = "Lnet/minecraft/client/font/TextRenderer;draw(Ljava/lang/String;FFIZLorg/joml/Matrix4f;Lnet/minecraft/client/render/VertexConsumerProvider;Lnet/minecraft/client/font/TextRenderer$TextLayerType;II)I"))
+    @Redirect(method = "render", at = @At(value = "INVOKE", target = "Lnet/minecraft/client/font/TextRenderer;draw(Ljava/lang/String;FFIZLnet/minecraft/client/util/math/Matrix4f;Lnet/minecraft/client/render/VertexConsumerProvider;Lnet/minecraft/client/font/TextRenderer$TextLayerType;II)I"))
     private int replaceCoordString(TextRenderer fr, String text, float x, float y, int color, boolean shadow, Matrix4f matrix, VertexConsumerProvider provider, TextRenderer.TextLayerType type, int light, int overlay) {
         if (MagicMix.coordinatesIsActive()) {
             String fake = String.format("X: %d  Z: %d", (int) MagicMix.getX(), (int) MagicMix.getZ());
@@ -22,7 +22,7 @@ public class XaeroCoordsMixin {
         return fr.draw(text, x, y, color, shadow, matrix, provider, type, light, overlay);
     }
 
-    @Redirect(method = "render", at = @At(value = "INVOKE", target = "Lnet/minecraft/client/font/TextRenderer;draw(Lnet/minecraft/text/OrderedText;FFIZLorg/joml/Matrix4f;Lnet/minecraft/client/render/VertexConsumerProvider;Lnet/minecraft/client/font/TextRenderer$TextLayerType;II)I"))
+    @Redirect(method = "render", at = @At(value = "INVOKE", target = "Lnet/minecraft/client/font/TextRenderer;draw(Lnet/minecraft/text/OrderedText;FFIZLnet/minecraft/client/util/math/Matrix4f;Lnet/minecraft/client/render/VertexConsumerProvider;Lnet/minecraft/client/font/TextRenderer$TextLayerType;II)I"))
     private int replaceCoordStringOrdered(TextRenderer fr, OrderedText text, float x, float y, int color, boolean shadow, Matrix4f matrix, VertexConsumerProvider provider, TextRenderer.TextLayerType type, int light, int overlay) {
         if (MagicMix.coordinatesIsActive()) {
             String fake = String.format("X: %d  Z: %d", (int) MagicMix.getX(), (int) MagicMix.getZ());
